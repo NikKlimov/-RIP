7fe9b6e0016ba9e919a617e2969dc7598c1042a4
klimov <klimov12345nikita@mail.ru>
2023-11-22 13:06:32 +0300

    22.11.23

--- bmstu/settings.py
+++ bmstu/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bmstu_lab.apps.BmstuLabConfig',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "bmstu_lab.AuthUser"
